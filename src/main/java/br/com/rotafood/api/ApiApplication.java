45bf4f8c46ceb75d7895d7ed56ce93050aefcbc4
ViniciusCostaGandolfi <vinicostagandolfi@gmail.com>
2024-10-10 13:49:48 -0300

    Commit Inicial

--- src/main/java/br/com/rotafood/api/ApiApplication.java
+++ src/main/java/br/com/rotafood/api/ApiApplication.java
@@ -12,7 +12,7 @@ public class ApiApplication {
 	public static void main(String[] args) throws IOException, URISyntaxException {
 
 		SpringApplication.run(ApiApplication.class, args);
-		System.out.println("\n\n\n Aplica\u00e7\u00e3o dispon\u00edvel em: \u001b[34mhttp://localhost:8080/swagger-ui.html\u001b[0m");
+		System.out.println("\n\n\n Aplica\u00e7\u00e3o dispon\u00edvel  em: \u001b[34mhttp://localhost:8080/swagger-ui.html\u001b[0m");
 	}
 
 }
