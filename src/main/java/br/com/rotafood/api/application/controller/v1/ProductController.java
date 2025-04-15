5c5a061acee8cf16e42a9a13a41f7a614b3836bc
ViniciusCostaGandolfi <vinicostagandolfi@gmail.com>
2025-04-15 14:01:45 -0300

    Adiciona nova rota de produtos

---
+++ src/main/java/br/com/rotafood/api/application/controller/v1/ProductController.java
@@ -0,0 +1,31 @@
+package br.com.rotafood.api.application.controller.v1;
+
+import br.com.rotafood.api.application.dto.catalog.ProductDto;
+import br.com.rotafood.api.application.service.catalog.ProductService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(ApiVersion.VERSION + "/merchants/{merchantId}/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+
+    @GetMapping
+    public ResponseEntity<List<ProductDto>> getAllProducts(@PathVariable UUID merchantId) {
+        List<ProductDto> products = productService.getAll(merchantId)
+                .stream()
+                .filter(product -> !"Pizza".equalsIgnoreCase(product.getName()))
+                .map(ProductDto::new)
+                .toList();
+        return ResponseEntity.ok(products);
+    }
+
+}
